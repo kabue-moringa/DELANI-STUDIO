70f51a95c47af3c9094e6de10dc8a5f5f09cdfc4
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-04-21 00:55:20 +0300

    add modified files

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
     $("#design").click(function () {
-      $("#img 1").toggle();
-      $(".design p").toggle();
+      $("#img 1").toggle('1500');
+      $(".design p").toggle('1000');
     });
     $("#development").click(function () {
-      $("#img 2").toggle();
-      $(".devp").toggle();
+      $("#img 2").toggle('1500');
+      $(".devp").toggle('1000');
     });
   
     $("#management").click(function () {
-      $("#img 3").toggle();
-      $(".prop").toggle();
+      $("#img 3").toggle('1500');
+      $(".prop").toggle('1000');
     });
   
   
@@ -26,11 +26,56 @@ $(document).ready(function () {
     } else {
       alert("Please fill all fields!");
     }
-  
-  })
-  
-  
-  
-  
-  
   });
+});
+$(document).ready(function(){
+  $("#first").hover(function(){
+    $("#overlay").hide();
+  }).hover(function(){
+    $("#overlay").show();
+  });
+});
+$(document).ready(function(){
+  $("#sec").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#pic3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#pic4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#pic5").mouseover(function(){
+    $("#overlay5").show();
+  }).mouseout(function(){
+    $("#overlay5").hide();
+  });
+  $("#pic6").mouseover(function(){
+    $("#overlay6").show();
+  }).mouseout(function(){
+    $("#overlay6").hide();
+  });
+  $("#pic7").mouseover(function(){
+    $("#overlay7").show();
+  }).mouseout(function(){
+    $("#overlay7").hide();
+  });
+  $("#pic8").mouseover(function(){
+    $("#overlay8").show();
+  }).mouseout(function(){
+    $("#overlay8").hide();
+  });
+});
